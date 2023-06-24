cce43bdcc5e968415246852c97ef26fee62b0649
yo1am1 <bigdiebam@gmail.com>
2023-06-24 04:42:25 +0300

    .

--- login/views.py
+++ login/views.py
@@ -6,18 +6,18 @@ from django.urls import reverse
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect("home")
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form, "user": request.user})
+    return render(request, "signup.html", {"form": form, "user": request.user})
 
 
 def login(request):
@@ -26,9 +26,7 @@ def login(request):
         if form.is_valid():
             username = request.POST["username"]
             password = request.POST["password"]
-            user = authenticate(
-                request, username=username, password=password
-            )
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect("home")
