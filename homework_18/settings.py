96e34bfc06f95ae38203744da94838d6683fd810
yo1am1 <bigdiebam@gmail.com>
2023-06-24 03:33:53 +0300

    .

--- homework_18/settings.py
+++ homework_18/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "login"
+    "login",
+    "bootstrap"
 ]
 
 MIDDLEWARE = [
