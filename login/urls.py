f909f35ae5f4ef2f6ac30c1af1931c1aeafe1e55
yo1am1 <bigdiebam@gmail.com>
2023-06-24 05:54:39 +0300

    .

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from . import views
-from .views import index, signup
-
+from .views import index, signup, activate
 
 urlpatterns = [
     path("", index, name="home"),
     path("register/", signup, name="signup"),
+    path(
+        r"^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
+        activate,
+        name="activate",
+    ),
 ]
--- login/views.py
+++ login/views.py
@@ -1,19 +1,35 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from login.forms import SignUpForm
+from django.core.mail import send_mail
+
+from login.tokens import account_activation_token
 
 
 def signup(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            user = form.save(commit=False)  # don't save to db yet
+            user.is_active = False
+            user.save()  # save to db
+            current_site = get_current_site(request)
+            message = render_to_string(
+                "account_activation_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                },
+            )
+            user.email_user("Validation", message)
             return redirect("home")
     else:
         form = SignUpForm()
@@ -25,3 +41,19 @@ def index(request):
         logout(request)
         return redirect("home")
     return render(request, "index.html", {"user": request.user})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64)
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect("home")
+    else:
+        return render(request, "account_activation_invalid.html")
